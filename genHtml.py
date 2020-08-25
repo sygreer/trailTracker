bb77391da4593d93001edb6c67fbf8b6c679f4ba
Sarah Greer <sygreer@mit.edu>
2020-08-24 18:21:07 -0700

    updated interface

--- genHtml.py
+++ genHtml.py
@@ -1,33 +1,133 @@
 from yattag import Doc
 from yattag import indent
+from datetime import timedelta
+from os import walk
+from os.path import splitext, exists
 
-doc, tag, text = Doc().tagtext()
+#data = ["IceLakeMatterhorn", "Ice Lake and Matterhorn"]
 
-with tag('html'):
-    with tag('head'):
-        with tag('title'):
-            text("Hiking | Sarah Greer")
-        doc.stag('link', rel="stylesheet", type="text/css", href="style.css", title="style") 
+def genHtml(data):
+    doc, tag, text = Doc().tagtext()
 
-    with tag('body'):
-        with tag('div', id = 'canvas_proj', style="width: 850px;"):
-            with tag('div', id='nav'):
-                with tag('a', href='../'):
-                    with tag('button', type='submit'):
-                        text("&#8592; Back home")
-            with tag("h1"):
-                text("Hiking and Travel")
-            doc.stag("br")
-            with tag("div", style="text-align:center;"):
-                doc.stag("iframe", src="map.html", width="850", height="800")
+    # get pictures
+    dirName = 'hikes/%s/'%data[0]
+    f = []
+    pics = []
+    for (dirpath, dirnames, filenames) in walk(dirName):
+        f.extend(filenames)
+    for fileN in f:
+        fn, fext = splitext(fileN)
+        if (fext.lower() == ".jpg" or fext.lower() ==".jpg"):
+            pics.append(fileN)
 
-            with tag('p', id = 'main'):
-                text('some text')
-            with tag('a', href='/my-url'):
-                text('some link')
+    with tag('html'):
+        with tag('head'):
+            with tag('title'):
+                #text("Hiking | Sarah Greer")
+                text("%s | Sarah Greer"%data[1])
+            doc.stag('link', rel="stylesheet", type="text/css", href="../../style.css", title="style") 
+            with tag("script", src="https://code.jquery.com/jquery-3.3.1.js", integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=", crossorigin="anonymous"):
+                text("")
+            with tag("script", type="text/javascript"):
+                text("function loadFrame (elm){var frame1 = document.getElementById('frame1');frame1.src = elm.dataset.src}")
 
-#result = indent(doc.getvalue())
-#print(result)
 
-with open('index.html', 'w') as f:
-    f.write(indent(doc.getvalue(), indent_text=True)) 
+
+    
+        with tag('body'):
+            with tag('div', id = 'canvas_proj', style="width: 850px;"):
+                with tag('div', id='nav'):
+                    with tag('a', href='../'):
+                        with tag('button', type='submit'):
+                            text("&#8592; Back home")
+                with tag("h1"):
+                    #text("Hiking and Travel")
+                    text(data[1])
+                doc.stag("br")
+                with tag("div", style="text-align:center;"):
+                    with tag("iframe", src="map.html", width="850", height="800"):
+                        text("")
+                    doc.stag("br")
+                    doc.stag("br")
+                    with tag('p'):
+                        with tag("h2"):
+                            text("Statistics")
+                        with tag("b"):
+                            text("Start time:")
+                        datestart = data[3] - timedelta(hours=7)
+                        date_time = datestart.strftime("%m/%d/%Y, %I:%M %p Pacific Time") 
+                        text(date_time)
+                        doc.stag("br")
+                        with tag("b"):
+                            text("End time:")
+                        dateend = data[4] - timedelta(hours=7)
+                        date_time = dateend.strftime("%m/%d/%Y, %I:%M %p Pacific Time") 
+                        text(date_time)
+                        doc.stag("br")
+                        with tag("b"):
+                            text("Total time:")
+                        text(data[2])
+                        doc.stag("br")
+                        doc.stag("br")
+                        with tag("b"):
+                            text("Total distance:")
+                        text("%s miles"%data[5])
+                        doc.stag("br")
+                        with tag("b"):
+                            text("Elevation change:")
+                        text("+%s feet, -%s feet"%(data[6], data[7]))
+                    doc.stag("br")
+                    doc.stag("br")
+
+                    with tag("h2"):
+                        text("Details")
+
+                    moreInfo= 'hikes/%s/more.html'%data[0]
+                    if (exists(moreInfo)):
+                        with tag("div", id="more"):
+                            text("")
+                    else:
+                        text("No further details have been added for this hike.")
+                    doc.stag("br")
+                    doc.stag("br")
+
+                    with tag("h2"):
+                        text("Photos")
+                    if (len(pics) ==0):
+                        text("No pictures have been added for this hike.")
+                    else:
+                        text("Click on the picture to view it full size.")
+                        doc.stag("br")
+                        for pic in pics:
+                            with tag("a", href=pic):
+                                doc.stag("img", src=pic, width="280px")
+
+                    doc.stag("br")
+                    doc.stag("br")
+                    with tag("h2"):
+                        text("Elevation profile")
+                    with tag("a", href="elev.html", target="eleviframe"):
+                        with tag('button', type='submit'):
+                            text("Click to load figure below.")
+                    with tag("iframe", name="eleviframe", src="about:blank", width="850", height="400"):
+                        text("")
+                    doc.stag("br")
+                    doc.stag("br")
+                    with tag("h2"):
+                        text("3D trip view")
+                    with tag("a", href="3d.html", target="myiFrame"):
+                        with tag('button', type='submit'):
+                            text("Click to load figure below.")
+                    with tag("iframe", name="myiFrame", src="about:blank", width="850", height="850"):
+                        text("")
+                    doc.stag("br")
+                    doc.stag("br")
+
+
+
+    with open('hikes/%s/index.html'%data[0], 'w') as f:
+        f.write(indent(doc.getvalue(), indent_text=True)) 
+
+
+genHtml(data)
+
--- visualizeGpx.py
+++ visualizeGpx.py
@@ -19,84 +19,118 @@ FT_TO_MI = 1.0/5280
 # -------------------------
 
 #fileName = '/home/sarah/sygreer/hiking/gpx/IceLakeMatterhorn.gpx'
-fileName = 'hikes/IceLakeMatterhorn/IceLakeMatterhorn.gpx'
-gpx_file = open(fileName, 'r')
-projName = os.path.splitext(os.path.basename(fileName))[0]
-dirName = os.path.dirname(fileName)
+#fileName = 'hikes/IceLakeMatterhorn/IceLakeMatterhorn.gpx'
+#fileName = 'hikes/WallowaMountainsLakesBasin/IceLakeMatterhorn.gpx'
 
-gpx = gpxpy.parse(gpx_file)
-nameRecording = gpx.name
+def analyzeGpx(name):
+    fileName = 'hikes/%s/%s.gpx'%(name,name)
+    gpx_file = open(fileName, 'r')
+    projName = os.path.splitext(os.path.basename(fileName))[0]
+    dirName = os.path.dirname(fileName)
+    
+    gpx = gpxpy.parse(gpx_file)
+    nameRecording = gpx.name
+    if (nameRecording == None):
+        nameRecording = projName
+    
+    #data = []
+    data = gpx.tracks[0].segments[0].points
+    for i in range(1, len(gpx.tracks[0].segments)):
+        data.extend(gpx.tracks[0].segments[i].points)
+    #data = gpx.tracks[0].segments[2].points
+    
+    ## Start Position
+    start = data[0]
+    ## End Position
+    finish = data[-1]
 
-#data = []
-data = gpx.tracks[0].segments[0].points
-for i in range(1, len(gpx.tracks[0].segments)):
-    data.extend(gpx.tracks[0].segments[i].points)
-#data = gpx.tracks[0].segments[2].points
+    df = pd.DataFrame(columns=['lon', 'lat', 'alt', 'time'])
+    for point in data:
+        df = df.append({'lon': point.longitude, 'lat' : point.latitude, 'alt' : point.elevation*M_TO_FT, 'time' : point.time}, ignore_index=True)
+    
+    #plt.plot(df['lon'], df['lat'])
+    #plt.show()
+    
+    alt_dif = [0]
+    time_dif = [0]
+    dist_hav = [0]
+    dist_hav_no_alt = [0]
+    dist_dif_hav_2d = [0]
 
-## Start Position
-start = data[0]
-## End Position
-finish = data[-1]
+    alt_inc = 0
+    alt_dec = 0
 
-df = pd.DataFrame(columns=['lon', 'lat', 'alt', 'time'])
-for point in data:
-    df = df.append({'lon': point.longitude, 'lat' : point.latitude, 'alt' : point.elevation*M_TO_FT, 'time' : point.time}, ignore_index=True)
+    # elevation moving average over 25 points
+    alt_inc25 = 0
+    alt_dec25 = 0
+    
+    for index in range(len(data)):
+        if index == 0:
+            pass
+        else:
+            start = data[index-1]
+            
+            stop = data[index]
+            
+            distance_hav_2d = haversine.haversine((start.latitude, start.longitude), (stop.latitude, stop.longitude))*1000
+            dist_dif_hav_2d.append(distance_hav_2d)
+            
+            dist_hav_no_alt.append(dist_hav_no_alt[-1] + distance_hav_2d)
+            
+            alt_d = (start.elevation - stop.elevation) * M_TO_FT
+            if (index%25==0):
+                startel = data[index-25].elevation 
+                dif_elev = (stop.elevation - startel) *M_TO_FT
+                if (dif_elev > 0):
+                    alt_inc25 = alt_inc25 + dif_elev
+                else:
+                    alt_dec25 = alt_dec25 - dif_elev
 
-#plt.plot(df['lon'], df['lat'])
-#plt.show()
+            if (alt_d > 0):
+                alt_inc = alt_inc + alt_d
+            else:
+                alt_dec = alt_dec - alt_d
+            
+            alt_dif.append(alt_d)
+            
+            distance_hav_3d = sqrt(distance_hav_2d**2 + (alt_d)**2) * M_TO_FT * FT_TO_MI
+                    
+            time_delta = (stop.time - start.time).total_seconds()
+            
+            time_dif.append(time_delta)
+                    
+            dist_hav.append(dist_hav[-1] + distance_hav_3d)
+    
+    df['dist_hav_2d'] = dist_hav_no_alt
+    df['dis_hav_3d'] = dist_hav
+    df['alt_dif'] = alt_dif
+    df['time_dif'] = time_dif
+    df['dis_dif_hav_2d'] = dist_dif_hav_2d
 
-alt_dif = [0]
-time_dif = [0]
-dist_hav = [0]
-dist_hav_no_alt = [0]
-dist_dif_hav_2d = [0]
+    df['spd'] = (df['dis_dif_hav_2d'] / df['time_dif']) * 3.6 * 0.621371 # speed in mph
+    
+    
+    #print('Haversine 3D : ', dist_hav[-1])
+    print('Distance : ', dist_hav[-1], " miles")
+    #print('Total Time : ', floor(sum(time_dif)/60),' min ', int(sum(time_dif)%60),' sec ')
+    timeTot = "%i hours, %i minutes" %(floor(sum(time_dif)/60/60), floor(sum(time_dif)/60%60))
+    print('Total Time : ', floor(sum(time_dif)/60/60),' hours ', floor(sum(time_dif)/60%60),' min ', int(sum(time_dif)%60),' sec ')
+    
+    fig = px.line_3d(df, x='lon', y='lat', z='alt', labels={'lon':'Longitude', 'lat':'Latitude', 'alt':'Elevation (feet)'})
+    #fig = px.Scatter3D(df, x='lon', y='lat', z='alt', labels={'lon':'Longitude', 'lat':'Latitude', 'alt':'Elevation (feet)'}, marker=dict(size=1, color=df['spd'], colorscale="Viridis"))
+    #fig.show()
+    #fig.write_html("%s_3d.html"%nameProj)
+    fig.write_html("%s/3d.html"%dirName)
+    
+    fig2 = px.line(df, x='dis_hav_3d', y='alt', labels={'dis_hav_3d':'Distance (miles)', 'alt':'Altitude (feet)'})
+    #fig2.write_html("%s_elev.html"%nameProj)
+    fig2.write_html("%s/elev.html"%dirName)
 
-for index in range(len(data)):
-    if index == 0:
-        pass
-    else:
-        start = data[index-1]
-        
-        stop = data[index]
-        
-        distance_hav_2d = haversine.haversine((start.latitude, start.longitude), (stop.latitude, stop.longitude))*1000
-        dist_dif_hav_2d.append(distance_hav_2d)
-        
-        dist_hav_no_alt.append(dist_hav_no_alt[-1] + distance_hav_2d)
-        
-        alt_d = start.elevation - stop.elevation
-        
-        alt_dif.append(alt_d)
-        
-        distance_hav_3d = sqrt(distance_hav_2d**2 + (alt_d)**2) * M_TO_FT * FT_TO_MI
-                
-        time_delta = (stop.time - start.time).total_seconds()
-        
-        time_dif.append(time_delta)
-                
-        dist_hav.append(dist_hav[-1] + distance_hav_3d)
+    return [name, nameRecording, timeTot, data[0].time, data[-1].time, "%.2f"%dist_hav[-1], "%.0f"%alt_inc25, "%.0f"%alt_dec25]
+    
 
-df['dist_hav_2d'] = dist_hav_no_alt
-df['dis_hav_3d'] = dist_hav
-df['alt_dif'] = alt_dif
-df['time_dif'] = time_dif
-df['dis_dif_hav_2d'] = dist_dif_hav_2d
+#data = analyzeGpx("IceLakeMatterhorn")
+data = analyzeGpx("WallowaMountainsLakesBasin")
+print(data)
 
-df['spd'] = (df['dis_dif_hav_2d'] / df['time_dif']) * 3.6 * 0.621371 # speed in mph
-
-
-#print('Haversine 3D : ', dist_hav[-1])
-print('Distance : ', dist_hav[-1], " miles")
-#print('Total Time : ', floor(sum(time_dif)/60),' min ', int(sum(time_dif)%60),' sec ')
-print('Total Time : ', floor(sum(time_dif)/60/60),' hours ', floor(sum(time_dif)/60%60),' min ', int(sum(time_dif)%60),' sec ')
-
-fig = px.line_3d(df, x='lon', y='lat', z='alt', labels={'lon':'Longitude', 'lat':'Latitude', 'alt':'Elevation (feet)'})
-#fig = px.Scatter3D(df, x='lon', y='lat', z='alt', labels={'lon':'Longitude', 'lat':'Latitude', 'alt':'Elevation (feet)'}, marker=dict(size=1, color=df['spd'], colorscale="Viridis"))
-#fig.show()
-#fig.write_html("%s_3d.html"%nameProj)
-fig.write_html("%s/3d.html"%dirName)
-
-fig2 = px.line(df, x='dis_hav_3d', y='alt', labels={'dis_hav_3d':'Distance (miles)', 'alt':'Altitude (feet)'})
-#fig2.write_html("%s_elev.html"%nameProj)
-fig2.write_html("%s/elev.html"%dirName)
 
